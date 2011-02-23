7d531716f1d08be2b4bba8dd97880eb034122848
Ricky Elrod <ricky@elrod.me>
2011-02-23 16:11:17 -0500

    Categorize pages.

--- w8upd/pages/models.py
+++ w8upd/pages/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return self.name
+
 class Page(models.Model):
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
     body = models.TextField()
     last_updated = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category)
 
     def __unicode__(self):
-        return self.title
+        return "[%s] %s" % (self.category.name, self.title)
 
