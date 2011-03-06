4ab2278c8b5bd85f9ad23d3923b9842f79b70679
Richard Elrod <n8sql@webvm01.w8upd.org>
2011-03-05 21:19:14 -0500

    ok people dont care what linux kernel we are running

--- w8upd/pages/context_processors.py
+++ w8upd/pages/context_processors.py
@@ -5,6 +5,6 @@ def versioninfo(request):
     python = sys.version.split()[0]
     osversion = os.uname()
     djangoversion = "%s.%s.%s" % (django.VERSION[0], django.VERSION[1], django.VERSION[2])
-    versions = "Django %s / Python %s / %s / %s %s %s" % \
-        (djangoversion, python, osversion[1].split('.')[0], osversion[0], osversion[2], osversion[4])
+    versions = "Django %s / Python %s / %s" % \
+        (djangoversion, python, osversion[1].split('.')[0])
     return {'versions': versions}
